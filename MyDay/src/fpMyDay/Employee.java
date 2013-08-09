a2ef673ee291b7ebe24800868e124da939f23137
unknown <shiggins@fre-shigginslt.cgx.net>
2013-08-09 21:57:08 +0100

    initial commit

---
+++ MyDay/src/fpMyDay/Employee.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpMyDay;
+
+/**
+ *
+ * @author shiggins
+ */
+public class Employee implements fpMyDay {
+    
+    public void startTime()
+    {
+    }
+    
+    public void endTime()
+    {
+    }
+    
+    public void getActiveApplication()
+    {
+    }
+    
+    public void printStatements()
+    {
+        System.out.println("User:" + emp_workstationName);
+    }
+    
+    String emp_workstationName;
+    String emp_department;
+    String emp_emailAddr;
+           
+    
+}
---
+++ MyDay/src/fpMyDay/fpMyDay.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpMyDay;
+
+/**
+ *
+ * @author shiggins
+ */
+public interface fpMyDay {
+    
+    void startTime();
+    void endTime();
+    void getActiveApplication();
+    void printStatements();
+    
+}
